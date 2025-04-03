7429bb62ba27eef33ae0e3b26e4b65702e0f4658
Martin Leonardo Antolini <martin.antolini@arba.gov.ar>
2025-04-03 12:51:36 -0300

    clase 3

---
+++ src/clase3/Ejercicios.java
@@ -0,0 +1,57 @@
+/**
+ * Ejercicio 1
+ *
+ * Calcular la suma de los primeros 100 números naturales.
+ *
+ * Ejercicio 2
+ *
+ * Imprimir los números pares del 1 al 50.
+ *
+ * Ejercicio 3
+ *
+ * Pedir ingresar números enteros (positivos o negativos) al usuario hasta que se
+ * ingrese el 0 e informar la cantidad de positivos y negativos ingresados.
+ *
+ * Ejercicio 4
+ *
+ * Pedir al usuario ingresar 5 números e informar cuál fue el mayor de esos 5.
+ * Hint: Integer.MIN_VALUE
+ *
+ * Ejercicio 5
+ *
+ * Pedir al usuario que ingrese las notas de un alumno (la carga finaliza al ingresar -1) e
+ * imprimir el promedio total de las notas.
+ *
+ * Ejercicio 6
+ *
+ * Escribe un programa que use break para detener un bucle cuando se encuentre el
+ * número 7 en un for del 1 al 10 que va imprimiendo los indices.
+ *
+ * Ejercicio 7
+ *
+ * Escribe un programa que use continue para omitir la impresión de números
+ * divisibles por 3 en un bucle for del 1 al 15.
+ *
+ * Ejercicio 8
+ *
+ * Usa un bucle while para sumar números consecutivos empezando desde el 1 pero
+ * detén el bucle si la suma supera 20 e imprime el resultado al terminar.
+ *
+ * Ejercicio 9
+ *
+ * Usa un bucle do-while para imprimir los números del 1 al 20, pero omite los
+ * múltiplos de 4.
+ *
+ * Ejercicio 10
+ *
+ * Escribir un programa que le pida al usuario adivinar un número generado
+ * aleatoriamente del 1 al 10. El programa termina una vez que el usuario adivine el
+ * número.
+ * Dar una pista al usuario cada vez que adivine erróneamente. La pista deberá ser si el
+ * número aleatorio es mayor o menor al ingresado.
+ */
+
+package clase3;
+
+public class Ejercicios {
+}
